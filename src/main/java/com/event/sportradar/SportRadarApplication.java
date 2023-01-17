dc17b1e7bd216d2c6364a52ec63c5f109b84d3cb
Lukasz Marchel <marchel.lukas@gmail.com>
2023-01-17 10:55:15 +0100

    first commit

---
+++ src/main/java/com/event/sportradar/SportRadarApplication.java
@@ -0,0 +1,13 @@
+package com.event.sportradar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportRadarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportRadarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/event/sportradar/SportRadarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.event.sportradar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportRadarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
